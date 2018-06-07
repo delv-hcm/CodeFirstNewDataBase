acc154b0353423880b6af0a484e7740b19826ef0
DELV <lede0510@gmail.com>
2018-06-07 21:29:15 +0700

    Initial commit

---
+++ Lingq/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lingq
+{
+    public class student
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = new int[5] { 1, 2, 3, 4, 5 };
+
+            List<student> Students = new List<student>()
+            {
+                new student { name = "xyz", id = 111 },
+                new student {name="abc",id=112},
+                 new student {name="abc",id=110}
+            };
+            var student_ = from stu in Students
+                           
+                           select stu.name;
+            var number = from num in list
+                         where num % 2 == 0
+                         select num;
+            foreach (var item in student_)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
